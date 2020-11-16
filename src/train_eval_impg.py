d0427d598a506fe3f9fbdf1c5e6ddcfb2c0bf614
Ch4nYH <hfercxx@hotmail.com>
2020-11-16 13:46:43 +0800

    update

--- src/train_eval_impg.py
+++ src/train_eval_impg.py
@@ -469,6 +469,8 @@ class Train_Eval(object):
                 if self.gen_masks is not None:
                     for k, m in enumerate(self.gen_model.modules()):
                         print(k)
+                        print(self.gen_masks[k].shape)
+                        print(m)
                         if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
                             m.weight_orig.grad.mul_(self.gen_masks[k])
 
