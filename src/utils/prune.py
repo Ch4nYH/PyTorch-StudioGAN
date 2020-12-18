61a9364e877f6a40a6b15985a377559f020136bb
Ch4nYH <hfercxx@hotmail.com>
2020-12-18 20:17:44 +0800

    update

--- src/utils/prune.py
+++ src/utils/prune.py
@@ -111,7 +111,7 @@ def pruning_generate_sn(model, px, initial_weight, parallel):
 def pruning_generate_extract(model, checkpoint, initial_weight, parallel):
     zero_flag = False
     masks = OrderedDict()
-    keys = model.state_dict().keys()
+    keys = list(odel.state_dict().keys())
     for k, m in enumerate(model.modules()):
         if isinstance(m, nn.Conv2d) or isinstance(m, nn.ConvTranspose2d):
             mask = checkpoint[keys[k]]
