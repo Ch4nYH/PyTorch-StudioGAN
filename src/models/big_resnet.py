669ba8a7f85b041988fff2c5b24ab4494620f601
Ch4nYH <hfercxx@hotmail.com>
2020-12-21 23:39:14 +0800

    update

--- src/models/big_resnet.py
+++ src/models/big_resnet.py
@@ -424,7 +424,7 @@ class Discriminator(nn.Module):
 
             elif self.conditional_strategy == 'ProjGAN_adv':
 
-                x_adv = h.detach().clone()
+                x_adv = h.clone().detach()
                 x_adv.requires_grad=True
                 
                 def adv_forward(g):
